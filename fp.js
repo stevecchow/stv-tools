6c1f70dd180090c37f2d5f75a80b600a80e1cfde
stevechow <zhmnda@icloud.com>
2021-06-27 20:46:12 +0800

    代码优化

--- fp.js
+++
@@ -1,2 +0,0 @@
-var _ = require('./stvTools.min').runInContext();
-module.exports = require('./fp/_baseConvert')(_, _);
--- fp/wrapperStvTools.js
+++
@@ -1,5 +0,0 @@
-var convert = require('./convert'),
-    func = convert('wrapperStvTools', require('../wrapperStvTools'), require('./_falseOptions'));
-
-func.placeholder = require('./placeholder');
-module.exports = func;
